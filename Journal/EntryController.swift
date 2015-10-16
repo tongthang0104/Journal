e04325cea0a911c56c5ce8b8c29887e3b0937b8e
Thang Tong <tongthang0104@gmail.com>
2015-10-15 23:09:35 -0600

    Update function add and remove entry

--- Journal/EntryController.swift
+++ Journal/EntryController.swift
@@ -8,14 +8,36 @@
 
 import Foundation
 
+// Define class EntryCotroller
 class EntryController {
     static let shareInstantEntry: EntryController = EntryController()
+   
+// Create Entry Array
     var entryArray = [Entry]()
     
     init () {
         self.entryArray = [Entry] ()
     }
  
+
+// Function let you add thing to your array
+
+    func addEntry (entryParameter: Entry) -> () {
+       self.entryArray.append(entryParameter)
+    
+}
+
+// func let you remove things from your array
+
+    func removeEntry (entryParameter2: Entry) -> () {
+    
+        let index = self.entryArray.indexOf(entryParameter2)
+        
+        if let entryIndex = index
+        {
+            self.entryArray.removeAtIndex(entryIndex)
+        }
+    }
 }
 
 
