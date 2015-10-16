b39c6509e441fc9f77571d2430e188f0c3e6ed5c
Thang Tong <tongthang0104@gmail.com>
2015-10-15 22:29:17 -0600

    Misc

---
+++ Journal/EntryController.swift
@@ -0,0 +1,13 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Thang H Tong on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    static let singleton = [Entry]()
+}
