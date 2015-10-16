f3dbc77eb2bb1b93ad804895fccc0d15bf688ac7
Thang Tong <tongthang0104@gmail.com>
2015-10-15 22:28:13 -0600

    Define Entry Class & its properties

---
+++ Journal/Entry.swift
@@ -0,0 +1,28 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Thang H Tong on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+struct Entry: Equatable {
+    // Create the properties
+    let timeStamps: NSDate
+    let title: String
+    let bodyText: String
+    
+    init (timeStamps: NSDate, title: String, bodyText: String) {
+        self.timeStamps = timeStamps
+        self.title = title
+        self.bodyText = bodyText
+    }
+}
+
+func ==(lhs: Entry, rhs: Entry) -> Bool {
+    return (lhs.bodyText == rhs.bodyText) && (lhs.timeStamps == rhs.timeStamps) && (lhs.title == rhs.title)
+}
+
+
